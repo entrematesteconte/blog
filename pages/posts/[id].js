8222374f8df5a5c60571f36cafd4983450bad38f
pedrodimarco <68877732+pedrodimarco@users.noreply.github.com>
2021-10-27 01:52:05 -0300

    social network sharing features

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,6 +5,7 @@ import Date from '../../components/date'
 import utilStyles from '../../styles/articles.module.css'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/contact'
+import Router from 'react-router'
 
 
 export async function getStaticProps({ params }) {
@@ -27,19 +28,25 @@ export async function getStaticPaths() {
 }
 
 
-
 export default function Post({ postData }) {
+    console.log(`https://entrematesteconte.vercel.app/posts/${postData.id}`)
     return (
         <>
             <Head>
                 <title>{postData.title}</title>
 
-                <meta property="og:url" content="http://localhost:3000/posts/ejemplo1" />
+                <meta property="og:url" content={`"https://entrematesteconte.vercel.app/posts/${postData.id}`} /> //Acá usar el url a compartir
                 <meta property="og:type" content="article" />
                 <meta property="og:title" content={postData.title} />
                 <meta property="og:description" content={postData.description} />
-                <meta property="og:image" content={postData.image} />
+                <meta property="og:image" content={`https://github.com/entrematesteconte/blog/blob/main/public${postData.image}?raw=true`} />
             </Head>
+
+            <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v12.0"
+                nonce="dHW9sZKe"></script>
+            <script src="https://platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
+
 
             <Navbar article></Navbar>
             <article className="container mt-5 mb-5" id="articulos">
@@ -54,7 +61,16 @@ export default function Post({ postData }) {
                     <Link href="/">
                         <a>← Volver a inicio</a>
                     </Link>
+                </div><br />
+
+                <div class="fb-share-button" data-href={`https://entrematesteconte.vercel.app/posts/${postData.id}`}
+                    data-layout="button_count" data-size="large"><a target="_blank"
+                        href={`https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fentrematesteconte.vercel.app%2Fposts%2F${postData.id}&amp;src=sdkpreparse`}
+                        class="fb-xfbml-parse-ignore">Compartir</a>
                 </div>
+
+                <br />
+                <script type="IN/Share" data-url={`https://entrematesteconte.vercel.app/posts/${postData.id}`}></script>
             </article>
 
 
